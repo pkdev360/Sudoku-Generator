7f7b9acf99a1f46f08480f20c985645cfd06d3cd
Pankaj Kumar <93446147+pkdev360@users.noreply.github.com>
2023-03-30 03:09:47 +0530

    Add files via upload

---
+++ sudoku_generator.py
@@ -0,0 +1,99 @@
+import copy
+import random as r
+import sudoku_solver_01 as solver
+
+
+board = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0]
+]
+
+
+def rand_box():
+    seq = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+    rand_row = r.choice(seq)
+    rand_col = r.choice(seq)
+    return rand_row, rand_col
+
+
+def filled_boxes(bo):
+    count = []
+    for i in range(len(bo)):
+        for j in range(len(bo[0])):
+            if bo[i][j] != 0:
+                count.append(1)
+    total = len(count)
+    return total
+
+
+def generate_board(bo, max_num):
+    if filled_boxes(bo) == max_num:
+        return True
+    while True:
+        rand_pos = rand_box()
+        row, col = rand_pos
+        if bo[row][col] == 0:
+            break
+        else:
+            continue
+
+    numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    rand_num = r.sample(numbers, 9)
+    for num in range(len(rand_num)):
+        if solver.valid(bo, rand_num[num], (row, col)):
+            bo[row][col] = rand_num[num]
+
+            if generate_board(bo, max_num):
+                return True
+
+            bo[row][col] = 0
+    return False
+
+
+while True:
+    generate_board(board, 15)
+
+    if solver.solve(board):
+        break
+
+solved_board = copy.deepcopy(board)
+
+
+def generate_board_2(solved_bo, empty_boxes):
+    length = 0
+    while length < empty_boxes:
+        numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+        rand_num_1 = r.choice(numbers)
+        rand_num_2 = r.choice(numbers)
+
+        box_value = solved_bo[rand_num_1][rand_num_2]
+        if box_value != 0:
+            solved_bo[rand_num_1][rand_num_2] = 0
+            length += 1
+        else:
+            continue
+
+
+def main():
+    generate_board_2(board, 30)
+
+    solver.display_board(solved_board)
+
+    print('\n*********************\n')
+
+    solver.display_board(board)
+
+    print('\n*********************\n')
+
+    solver.display_board_list(board)
+
+
+if __name__ == "__main__":
+    main()
---
+++ sudoku_solver_01.py
@@ -0,0 +1,114 @@
+
+# board = [
+#         [0, 0, 0, 0, 0, 0, 0, 0, 0],
+#         [0, 0, 0, 0, 0, 0, 0, 0, 0],
+#         [0, 0, 0, 0, 0, 0, 0, 0, 0],
+#         [0, 0, 0, 0, 0, 0, 0, 0, 0],
+#         [0, 0, 0, 0, 0, 0, 0, 0, 0],
+#         [0, 0, 0, 0, 0, 0, 0, 0, 0],
+#         [0, 0, 0, 0, 0, 0, 0, 0, 0],
+#         [0, 0, 0, 0, 0, 0, 0, 0, 0],
+#         [0, 0, 0, 0, 0, 0, 0, 0, 0]
+# ]
+
+board = [
+        [5, 3, 0, 0, 7, 0, 0, 0, 0],
+        [6, 0, 0, 1, 9, 5, 0, 0, 0],
+        [0, 9, 8, 0, 0, 0, 0, 6, 0],
+        [8, 0, 0, 0, 6, 0, 0, 0, 3],
+        [4, 0, 0, 8, 0, 3, 0, 0, 1],
+        [7, 0, 0, 0, 2, 0, 0, 0, 6],
+        [0, 6, 0, 0, 0, 0, 2, 8, 0],
+        [0, 0, 0, 4, 1, 9, 0, 0, 5],
+        [0, 0, 0, 0, 8, 0, 0, 7, 9]
+]
+
+
+def display_board(bo):
+    for i in range(len(bo)):
+        if i % 3 == 0 and i != 0:
+            print('- - - - - - - - - - -')
+        for j in range(len(bo[0])):
+            if j % 3 == 0 and j != 0:
+                print('| ', end='')
+
+            if j == 8:
+                print(bo[i][j])
+            else:
+                print(str(bo[i][j]) + ' ', end='')
+
+
+def display_board_list(bo):
+    print('board = [')
+    for i in range(len(bo)):
+        for j in range(len(bo[0])):
+            if j == 0:
+                print('\t[' + str(bo[i][j]) + ', ', end='')
+            elif j == 8 and i != 8:
+                print(str(bo[i][j]) + '],')
+            elif i == 8 and j == 8:
+                print(str(bo[i][j]) + ']')
+            else:
+                print(str(bo[i][j]) + ', ', end='')
+    print(']')
+
+
+def find_empty(bo):
+    for i in range(len(bo)):
+        for j in range(len(bo[0])):
+            if bo[i][j] == 0:
+                return (i, j)  # row, col
+    return False
+
+
+def valid(bo, num, pos):
+    for i in range(len(bo)):
+        for j in range(len(bo[0])):
+            if bo[pos[0]][j] == num and pos[1] != j:
+                return False
+
+    for i in range(len(bo)):
+        for j in range(len(bo[0])):
+            if bo[i][pos[1]] == num and pos[0] != i:
+                return False
+
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+
+    for i in range(box_y * 3, box_y * 3 + 3):
+        for j in range(box_x * 3, box_x * 3 + 3):
+            if bo[i][j] == num and (i, j) != pos:
+                return False
+
+    return True
+
+
+def solve(bo):
+    find = find_empty(bo)
+    if not find:
+        return True
+    else:
+        row, col = find
+
+    for num in range(1, 10):
+        if valid(bo, num, (row, col)):
+            bo[row][col] = num
+
+            if solve(bo):
+                return True
+            else:
+                bo[row][col] = 0
+    return False
+
+
+def main():
+    display_board(board)
+    print('\n*********************\n')
+
+    solve(board)
+
+    display_board(board)
+
+
+if __name__ == "__main__":
+    main()
